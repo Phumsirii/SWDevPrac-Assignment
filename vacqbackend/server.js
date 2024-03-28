7f664086a268c68db37f5b5d5f1b8d17603ce999
unknown <phumsirisumativit@gmail.com>
2024-03-28 15:21:36 +0700

    add: Home,Login,Register and Header pages

---
+++ vacqbackend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
+const connectDB=require('./config/db');
+
+dotenv.config({path:'./config/config.env'});
+
+connectDB();
+
+const app=express();
+
+//Body Parser
+app.use(express.json());
+
+//Cookie Parser
+app.use(cookieParser());
+
+//Route files
+const hospitals = require('./routes/hospitals');
+const appointments=require('./routes/appointments');
+
+const auth = require('./routes/auth');
+
+//Mount routers
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments',appointments);
+
+const PORT = process.env.PORT || 5000;
+const server=app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,' mode on port ',PORT));
+
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error:${err.message}`);
+    server.close(()=>process.exit(1));
+});
