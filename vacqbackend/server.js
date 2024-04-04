e0b5c439303ec5c602719b2bac85512d0787218f
unknown <phumsirisumativit@gmail.com>
2024-04-04 21:00:15 +0700

    feat:OPENAPI via Swagger

--- vacqbackend/server.js
+++ vacqbackend/server.js
@@ -8,6 +8,8 @@ const {xss}=require('express-xss-sanitizer');
 const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const cors=require('cors');
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUI=require('swagger-ui-express');
 
 dotenv.config({path:'./config/config.env'});
 
@@ -63,3 +65,23 @@ process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error:${err.message}`);
     server.close(()=>process.exit(1));
 });
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Library API',
+            version:'1.0.0',
+            description:'A simple Express VacQ API'
+        },
+        servers:[
+            {
+                url:'http://localhost:5000/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
